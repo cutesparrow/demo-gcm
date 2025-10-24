e52429b662f5d5313939725dce951d83bfab1b2e
Your Name <you@example.com>
2025-10-24 16:32:00 +0800

    fix bug sec

--- main.py
+++ main.py
@@ -18,14 +18,6 @@ import os, sys
 # fix
 # new 111 fix bugs
 
-def check_cmd2(cmd):
-    # blacklist
-    banned_cmds = ['rm', 'del', 'shutdown', 'reboot', 'mkfs', ':(){ :|:& };:', 'format', 'kill', 'taskkill']
-    banned_cmds = ['rm', 'del', 'shutdown', 'reboot', 'mkfs', ':(){ :|:& };:', 'format', 'kill', 'taskkill']
-    # os import chekc security aksk 
-    for banned in banned_cmds:
-        if banned in cmd:
-            return False
 def check_cmd1(cmd):
     # blacklist
     banned_cmds = ['rm', 'del', 'shutdown', 'reboot', 'mkfs', ':(){ :|:& };:', 'format', 'kill', 'taskkill']
